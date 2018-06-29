6ceb92b7675b59536e415806885840e7d720e698
Patrick Schadler <patrick.schadler@gmail.com>
2018-06-29 22:17:47 +0200

    feat(sensor-data): init commit. add podfile for alamofire configuration. add button and labels to provide interaction and results.

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Sensor Visualization' do
+pod 'Alamofire', '~> 4.7'
+end
--- Sensor Visualization/ViewController.swift
+++ Sensor Visualization/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func refreshDataButtonClicked(_ sender: UIButton) {
+        refreshSensorData();
+    }
+    
+    func refreshSensorData(){
+        self.temperatureLabel.text = "Refresh Temp"
+        self.humidityLabel.text = "Refresh Humidity"
+        makeRestCall();
+    }
+    
+    func makeRestCall(){
+        let url = URL(string: "http://192.168.1.43:3000/sensordata")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                    
+                    if let json = jsonSerialized, let url = json["url"], let explanation = json["explanation"] {
+                        print(url)
+                        print(explanation)
+                    }
+                }  catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+    }
 }
-
