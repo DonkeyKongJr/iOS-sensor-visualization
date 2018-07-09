6bc114d46ad3d63e2f982b4c6584236f358a04b3
Patrick Schadler <patrick.schadler@gmail.com>
2018-07-09 20:53:37 +0200

    add humidity chart and limit charts to display first 24 values

--- Sensor Visualization/SensorDataChartController.swift
+++ Sensor Visualization/SensorDataChartController.swift
@@ -13,6 +13,7 @@ import FirebaseFirestore
 
 class SensorDataChartController: UIViewController {
     @IBOutlet weak var lineChart: Chart!
+    @IBOutlet weak var humidityChart: Chart!
     
     @IBAction func backButton(_ sender: UIButton) {
         self.dismiss(animated: true)
@@ -21,32 +22,50 @@ class SensorDataChartController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.lineChart.minY = 0
-        self.lineChart.maxY = 40
-        self.getDataFromFirestore();
+        self.setChartMetaData()
+        self.getDataFromFirestore()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setChartMetaData(){
+        self.lineChart.minY = 0
+        self.lineChart.maxY = 40
+        self.humidityChart.minY = 0;
+        self.humidityChart.maxY = 100;
+    }
 
     func getDataFromFirestore(){
         let db = Firestore.firestore()
         var temperatures = [Double]()
+        var humidities = [Double]()
         
         db.collection("sensordata").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
                 for document in querySnapshot!.documents {
-                    print("\(document.documentID) => \(document.data())")
+                    if(temperatures.count >= 24){
+                        break
+                    }
                     
                     let data = document.data() as NSDictionary
                     temperatures.append(data["temperature"] as! Double)
+                    humidities.append(data["humidity"] as! Double)
                 }
+                
                 let series = ChartSeries(temperatures)
                 self.lineChart.add(series)
+                self.lineChart.yLabelsFormatter = { String(Int($1)) +  "ºC" }
+                self.lineChart.xLabelsFormatter = { String(Int(round($1)))}
+                
+                let humiditySeries = ChartSeries(humidities)
+                self.humidityChart.add(humiditySeries)
+                self.humidityChart.yLabelsFormatter = { String(Int($1)) +  "%" }
+                self.humidityChart.xLabelsFormatter = { String(Int(round($1)))}
             }
         }
     }
